3826a77979edf5ea8be98a6de9262118f86813bd
HassanAbbas7 <ph015016test1@gmail.com>
2026-04-10 16:26:22 +0500

    a

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/twitter-api': {
-        target: 'https://api.twitterapi.io',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/twitter-api/, ''),
-      },
-    },
-  },
 })
